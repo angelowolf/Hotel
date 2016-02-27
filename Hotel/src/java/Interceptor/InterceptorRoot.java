a8fcb8597b4b2403d0186bb3bbafd71824b25243
angelowolf <angelowolf21@gmail.com>
2016-02-27 18:57:33 -0300

    Interceptores root y owner

--- Hotel/src/java/Interceptor/InterceptorRoot.java
+++ Hotel/src/java/Interceptor/InterceptorRoot.java
@@ -1,5 +1,6 @@
 package Interceptor;
 
+import Persistencia.Modelo.Usuario;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
@@ -9,15 +10,15 @@ import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
  * @version 1.0
  * @created 28-ene-2016 08:44:26 p.m.
  */
-public class in extends AbstractInterceptor {
+public class InterceptorRoot extends AbstractInterceptor {
 
-    private static final long serialVersionUID = 1L;
 
     @Override
     public String intercept(ActionInvocation actionInvocation) throws Exception {
 
         String result = Action.LOGIN;
-        if (actionInvocation.getInvocationContext().getSession().containsKey("user")) {
+        Usuario u = (Usuario) actionInvocation.getInvocationContext().getSession().get("user");
+        if (u!= null && u.isRoot()) {
             result = actionInvocation.invoke();
         }
         return result;
